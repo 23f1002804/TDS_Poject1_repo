8f7c73e889e850b53fee944dde281289b514a369
23f1002804 <23f1002804@ds.study.iitm.ac.in>
2025-06-12 13:02:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -725,6 +725,10 @@ async def health_check():
         )
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True) 
+    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+
+@app.get("/")
+def root():
+    return {"message": "Welcome"}
 
 
